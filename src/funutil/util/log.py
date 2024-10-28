31c4b6eefad08b15fc8aef85a587026f9e7c1cf8
farfun <1007530194@qq.com>
2024-10-28 15:48:20 +0800

    add

--- src/funutil/util/log.py
+++ src/funutil/util/log.py
@@ -9,9 +9,11 @@ def get_logger(
     logger = logging.getLogger(name)
     if handler is None:
         handler = logging.StreamHandler()
+        handler.setLevel(level=level)
         handler.setFormatter(logging.Formatter(formatter))
     logger.addHandler(handler)
     logger.setLevel(level=level)
+
     logger.info(
         f"init logger with name={name} and level={level}",
     )
