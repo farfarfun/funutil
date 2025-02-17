2008898c64a18e7bb5d31cb84dbde61b5c47d049
farfun <1007530194@qq.com>
2025-02-17 11:18:06 +0800

    add

--- src/funutil/__init__.py
+++ src/funutil/__init__.py
@@ -1,10 +1,11 @@
 from .time import RunTimer, run_timer
-from .util import deep_get, get_logger, get_package_version, getLogger
+from .util import deep_get, get_logger, get_package_version, getLogger, find_get
 
 __all__ = [
     "RunTimer",
     "deep_get",
     "get_logger",
+    "find_get",
     "getLogger",
     "run_timer",
     "get_package_version",
